547eed0f3a8f1ad1d9f1aac64dd730608533e39d
schnell3526 <schnelldickheiter@gmail.com>
2022-10-31 13:37:15 +0900

    initial commit

--- pokemon-app/src/App.tsx
+++ pokemon-app/src/App.tsx
@@ -1,48 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-import { getAllPokemon } from './utils/pokemon'
+import { getAllPokemon,getPokemon } from './utils/pokemon'
+import Card from './compoments/Card/Card';
 
 function App() {
   const initialURL: string = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([] as any[])
 
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // 全てのポケモンデータを取得
       let res = await getAllPokemon(initialURL);
-      console.log(res);
+      // 各ポケモンの詳細なデータを表示
+      loadPokemon(res.results);
+      // console.log(res.results);
       setLoading(false);
+
     }
     fetchPokemonData()
   }, [])
 
+  const loadPokemon = async (data: any) => {
+    let _pokemonData = await Promise.all(
+      data.map((pokemon: any) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord
+      })
+    )
+    setPokemonData(_pokemonData)
+  };
+
+  console.log(pokemonData)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          {loading ? (
-            <h1>ロード中・・・</h1>
-          ) : (
-            <h1>ポケモンデータを取得しました。</h1>
-          )}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <h2>ロード中・・・</h2>
+      ) : (
+        <>
+          <div className='pokemonCardContainer'>
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon}/>;
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
--- pokemon-app/src/utils/pokemon.ts
+++ pokemon-app/src/utils/pokemon.ts
@@ -1,7 +1,19 @@
-export const getAllPokemon = (url: string) => {
+export function getAllPokemon(
+    url: string
+): Promise<any> {
     return new Promise((resolve, reject) => { // resolve関数の引数にすることで成功を表す
         fetch(url)
         .then((res) => res.json())
         .then((data) => resolve(data));
     });
 }
+
+export const getPokemon = (url: string) => {
+    return new Promise((resolve, reject) => {
+        fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            resolve(data);
+        });
+    });
+};
